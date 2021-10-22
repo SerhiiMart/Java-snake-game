006578d7a260acecec89c0c1d696ec17e61fc168
Serhii <malgothsm@gmail.com>
2021-10-22 19:24:18 +0300

    Working on game panel update

--- src/snakegame/SnakeGamePanel.java
+++ src/snakegame/SnakeGamePanel.java
@@ -3,6 +3,9 @@ package snakegame;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
@@ -21,11 +24,29 @@ public class SnakeGamePanel extends JPanel implements ActionListener {
 	public void draw(Graphics g) {
 		
 	}
+	public void move() {
+		
+	}
+	public void checkBall() {
+		
+	}
+	public void checkCollision() {
+		
+	}
+	public void gameOver(Graphics g) {
+		
+	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
+	}
+	public class MyKeyAdapter extends KeyAdapter {
+		@Override
+		public void keyPressed(KeyEvent e) {
+			
+		}
 	}
 
 }
