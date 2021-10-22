d905f13c03580cc07700c47c44a2724a2ac5cda4
Serhii <malgothsm@gmail.com>
2021-10-22 19:05:33 +0300

    Working on game panel

--- src/snakegame/GameFrame.java
+++ src/snakegame/GameFrame.java
@@ -10,5 +10,8 @@ public class GameFrame extends JFrame {
 		this.setTitle("Snake");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
 	}
 }
--- src/snakegame/SnakeGamePanel.java
+++ src/snakegame/SnakeGamePanel.java
@@ -1,14 +1,27 @@
 package snakegame;
 
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
 
 public class SnakeGamePanel extends JPanel implements ActionListener {
+	
 	SnakeGamePanel(){
 		
 	}
+	public void startGame() {
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+	}
+	public void draw(Graphics g) {
+		
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
