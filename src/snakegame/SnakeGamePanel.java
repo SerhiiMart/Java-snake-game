1d19b6ee402e21d8863e51a4392de12e1e84927f
Serhii <malgothsm@gmail.com>
2021-10-24 16:45:03 +0300

    Working on keyPressed class

--- src/snakegame/SnakeGamePanel.java
+++ src/snakegame/SnakeGamePanel.java
@@ -140,7 +140,30 @@ public class SnakeGamePanel extends JPanel implements ActionListener {
 	public class MyKeyAdapter extends KeyAdapter {
 		@Override
 		public void keyPressed(KeyEvent e) {
-			
+			switch (e.getKeyCode()) {
+			case KeyEvent.VK_LEFT: 
+				if(direction != 'R') {
+					direction = 'L';
+				}
+				break;
+			case KeyEvent.VK_RIGHT: 
+				if(direction != 'L') {
+					direction = 'R';
+				}
+				break;
+			case KeyEvent.VK_UP: 
+				if(direction != 'D') {
+					direction = 'U';
+				}
+				break;
+			case KeyEvent.VK_DOWN: 
+				if(direction != 'U') {
+					direction = 'D';
+				}
+				break;
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + e.getKeyCode());
+			}
 		}
 	}
 
